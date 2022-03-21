02f6f5c37132af4dfb50c076862781dd840e3c6c
irfantriadiginting <irfan.triadi@ti.ukdw.ac.id>
2022-03-21 22:57:33 +0700

    pertemuan6

---
+++ pertemuan6_71190508/app/src/main/java/id/ac/ukdw/pertemuan6_71190508/FragmentA.kt
@@ -0,0 +1,28 @@
+package id.ac.ukdw.pertemuan6_71190508
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class FragmentA: Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val viewA = inflater.inflate(R.layout.fragment_a, container, false)
+        val buttonA = viewA.findViewById<Button>(R.id.buttonA)
+
+        buttonA.setOnClickListener {
+            val i = Intent(context, Page2Activity::class.java);
+            startActivity(i)
+            Toast.makeText(context,"Anda sekarang berada di Halaman 2",Toast.LENGTH_SHORT).show()
+        }
+        return viewA
+    }
+}
---
+++ pertemuan6_71190508/app/src/main/java/id/ac/ukdw/pertemuan6_71190508/Page3Activity.kt
@@ -0,0 +1,11 @@
+package id.ac.ukdw.pertemuan6_71190508
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Page3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page3)
+    }
+}
