33fe3b45d270074a344474f0a875887339bc78b7
irfantriadiginting <irfan.triadi@ti.ukdw.ac.id>
2022-04-18 21:25:19 +0700

    pertemuan7

--- pertemuan7_71190508/app/src/main/java/id/ac/ukdw/pertemuan7_71190508/Contact.kt
+++
@@ -1,3 +0,0 @@
-package id.ac.ukdw.pertemuan7_71190508
-
-data class Contact(val contactName: String, val contactNumber: String, val contactImage: Int, val contactAddress: String, val contactEmail: String)
